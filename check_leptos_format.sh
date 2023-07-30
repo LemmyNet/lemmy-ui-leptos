81193ab8b20c5a5b172539ef60b39777f4eda1a2
Dessalines <tyhou13@gmx.com>
2023-07-30 10:44:47 -0400

    Try alternate script run 9.

--- check_leptos_format.sh
+++ check_leptos_format.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 # Copy old src to /tmp
-cp -R src /tmp/lemmysrc
+mkdir .tmp
+cp -R src .tmp/lemmysrc
 
 # Run leptosfmt
 leptosfmt -c .leptosfmt.toml src
 
 # Diff the dirs
-diff -r src /tmp/lemmysrc/src
+diff -r src .tmp/lemmysrc/src
+rm -rf .tmp
