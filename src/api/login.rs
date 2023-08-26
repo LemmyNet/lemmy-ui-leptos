042d443494442dc854591a536130c6c61da255b6
SleeplessOne1917 <abias1122@gmail.com>
2023-08-26 07:48:49 -0400

    Abstract api function details

--- src/api/login.rs
+++ src/api/login.rs
@@ -1,10 +1,4 @@
-use crate::{
-  api::{api_wrapper, HttpType},
-  errors::LemmyAppError,
-};
+use crate::api_fn;
 use lemmy_api_common::person::{Login, LoginResponse};
-use leptos::Scope;
 
-pub async fn login(cx: Scope, form: &Login) -> Result<LoginResponse, LemmyAppError> {
-  api_wrapper::<LoginResponse, Login>(cx, HttpType::Post, "user/login", form).await
-}
+api_fn!(login, Login, LoginResponse, Post, "user/login");
--- src/api/post.rs
+++ src/api/post.rs
@@ -1,12 +1,5 @@
-use super::HttpType;
-use crate::{api::api_wrapper, errors::LemmyAppError};
+use crate::api_fn;
 use lemmy_api_common::post::{GetPost, GetPostResponse, GetPosts, GetPostsResponse};
-use leptos::Scope;
 
-pub async fn list_posts(cx: Scope, form: &GetPosts) -> Result<GetPostsResponse, LemmyAppError> {
-  api_wrapper::<GetPostsResponse, GetPosts>(cx, HttpType::Get, "post/list", form).await
-}
-
-pub async fn get_post(cx: Scope, form: &GetPost) -> Result<GetPostResponse, LemmyAppError> {
-  api_wrapper::<GetPostResponse, GetPost>(cx, HttpType::Get, "post", form).await
-}
+api_fn!(list_posts, GetPosts, GetPostsResponse, Get, "post/list");
+api_fn!(get_post, GetPost, GetPostResponse, Get, "post");
