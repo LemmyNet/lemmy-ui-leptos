747df87f612a032165dc64eb3a767bda0baf4afb
SleeplessOne1917 <abias1122@gmail.com>
2023-08-26 02:42:36 -0400

    Unify API wrapper

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,4 +1,5 @@
 use crate::errors::LemmyAppError;
+use cfg_if::cfg_if;
 use leptos::{Scope, Serializable};
 use serde::Serialize;
 use serde_json::Value;
@@ -12,69 +13,8 @@ const ENDPOINT: &str = "https://voyager.lemmy.ml/api/v3";
 pub enum HttpType {
   Get,
   Post,
+  #[allow(dead_code)]
   Put,
-}
-
-#[cfg(not(feature = "ssr"))]
-pub async fn api_wrapper<Response, Form>(
-  cx: Scope,
-  type_: HttpType,
-  path: &str,
-  form: &Form,
-) -> Result<Response, LemmyAppError>
-where
-  Response: Serializable,
-  Form: Serialize,
-{
-  use wasm_bindgen::UnwrapThrowExt;
-
-  let route = &build_route(path);
-  let abort_controller = web_sys::AbortController::new().ok();
-  let abort_signal = abort_controller.as_ref().map(|a| a.signal());
-
-  let json = match type_ {
-    HttpType::Get => {
-      gloo_net::http::Request::get(&build_fetch_query(route, form))
-        .abort_signal(abort_signal.as_ref())
-        .send()
-        .await?
-        .text()
-        .await?
-    }
-    HttpType::Post => {
-      gloo_net::http::Request::post(route)
-        .json(form)
-        .expect_throw("Could not parse json body")
-        .abort_signal(abort_signal.as_ref())
-        .send()
-        .await?
-        .text()
-        .await?
-    }
-    HttpType::Put => {
-      gloo_net::http::Request::put(route)
-        .json(form)
-        .expect_throw("Could not parse json body")
-        .abort_signal(abort_signal.as_ref())
-        .send()
-        .await?
-        .text()
-        .await?
-    }
-  };
-
-  // abort in-flight requests if the Scope is disposed
-  // i.e., if we've navigated away from this page
-  leptos::on_cleanup(cx, move || {
-    if let Some(abort_controller) = abort_controller {
-      abort_controller.abort()
-    }
-  });
-
-  // Return the error response json as an error
-  Response::de(&json).map_err(|_| LemmyAppError::APIError {
-    error: json_deser_err(&json),
-  })
 }
 
 /// Used if you hit a deser error, which usually means a LemmyAPI error
@@ -85,7 +25,6 @@ fn json_deser_err(json: &str) -> String {
     .unwrap_or("Unknown".to_string())
 }
 
-#[cfg(feature = "ssr")]
 pub async fn api_wrapper<Response, Form>(
   _cx: Scope,
   type_: HttpType,
@@ -97,20 +36,70 @@ where
   Form: Serialize,
 {
   let route = &build_route(path);
-  let client = reqwest::Client::new();
+  let json;
 
-  let json = match type_ {
-    HttpType::Get => {
-      client
-        .get(&build_fetch_query(route, form))
-        .send()
-        .await?
-        .text()
-        .await?
-    }
-    HttpType::Post => client.post(path).json(form).send().await?.text().await?,
-    HttpType::Put => client.put(path).json(form).send().await?.text().await?,
-  };
+  cfg_if! {
+    if #[cfg(feature = "ssr")] {
+     let client = reqwest::Client::new();
+
+     json = match type_ {
+       HttpType::Get => {
+         client
+           .get(&build_fetch_query(route, form))
+           .send()
+           .await?
+           .text()
+           .await?
+       }
+       HttpType::Post => client.post(path).json(form).send().await?.text().await?,
+       HttpType::Put => client.put(path).json(form).send().await?.text().await?,
+     };
+   } else {
+     use wasm_bindgen::UnwrapThrowExt;
+
+     let abort_controller = web_sys::AbortController::new().ok();
+     let abort_signal = abort_controller.as_ref().map(|a| a.signal());
+
+     json = match type_ {
+       HttpType::Get => {
+         gloo_net::http::Request::get(&build_fetch_query(route, form))
+           .abort_signal(abort_signal.as_ref())
+           .send()
+           .await?
+           .text()
+           .await?
+       }
+       HttpType::Post => {
+         gloo_net::http::Request::post(route)
+           .json(form)
+           .expect_throw("Could not parse json body")
+           .abort_signal(abort_signal.as_ref())
+           .send()
+           .await?
+           .text()
+           .await?
+       }
+       HttpType::Put => {
+         gloo_net::http::Request::put(route)
+           .json(form)
+           .expect_throw("Could not parse json body")
+           .abort_signal(abort_signal.as_ref())
+           .send()
+           .await?
+           .text()
+           .await?
+       }
+     };
+
+     // abort in-flight requests if the Scope is disposed
+     // i.e., if we've navigated away from this page
+     leptos::on_cleanup(_cx, move || {
+       if let Some(abort_controller) = abort_controller {
+         abort_controller.abort()
+       }
+     });
+   }
+  }
 
   // Return the error response json as an error
   Response::de(&json).map_err(|_| LemmyAppError::APIError {
@@ -119,10 +108,10 @@ where
 }
 
 fn build_route(route: &str) -> String {
-  format!("{}/{}", ENDPOINT, route)
+  format!("{ENDPOINT}/{route}")
 }
 
 fn build_fetch_query<T: Serialize>(path: &str, form: T) -> String {
   let form_str = serde_urlencoded::to_string(&form).unwrap_or(path.to_string());
-  format!("{}?{}", path, form_str)
+  format!("{path}?{form_str}")
 }
