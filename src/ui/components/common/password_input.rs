4db4f662d48afc218003d38e2759a5e85d945617
SleeplessOne1917 <abias1122@gmail.com>
2023-08-31 08:01:07 -0400

    Remove action logic from login activity

--- src/ui/components/common/password_input.rs
+++ src/ui/components/common/password_input.rs
@@ -2,11 +2,12 @@ use leptos::*;
 use leptos_heroicons::size_24::outline::{Eye, EyeSlash};
 
 #[component]
-pub fn PasswordInput(
+pub fn PasswordInput<F: Fn(String) + 'static>(
   cx: Scope,
   #[prop(optional)] disabled: MaybeProp<bool>,
   #[prop(into)] id: &'static str,
   #[prop(into)] name: &'static str,
+  on_input: F,
 ) -> impl IntoView {
   let (show_password, set_show_password) = create_signal(cx, false);
 
@@ -19,13 +20,17 @@ pub fn PasswordInput(
       </label>
       <div class="join">
         <input
-          type=move || if show_password() { "text " } else { "password" }
+          type=move || show_password.with(|s| if *s { "text " } else { "password" })
           id=id
           class="input input-bordered join-item w-full"
           required
           name=name
           disabled=move || disabled.get().unwrap_or(false)
+          on:input=move |e| {
+              on_input(event_target_value(&e));
+          }
         />
+
         <button
           type="button"
           class="btn btn-outline join-item rounded-r-full btn-primary"
--- src/ui/components/login/login_form.rs
+++ src/ui/components/login/login_form.rs
@@ -33,19 +33,12 @@ pub async fn login(
 }
 
 #[component]
-pub fn LoginForm(
-  cx: Scope,
-  action: Action<(String, String), ()>,
-  error: Signal<Option<String>>,
-  disabled: Signal<bool>,
-) -> impl IntoView {
+pub fn LoginForm(cx: Scope) -> impl IntoView {
   let (password, set_password) = create_signal(cx, String::new());
   let (name, set_name) = create_signal(cx, String::new());
 
-  let dispatch_action = move || action.dispatch((name.get(), password.get()));
-
   let button_is_disabled = Signal::derive(cx, move || {
-    disabled() || password.with(|p| p.is_empty()) || name.with(|n| n.is_empty())
+    password.with(|p| p.is_empty()) || name.with(|n| n.is_empty())
   });
 
   let login = create_server_action::<LoginForm>(cx);
@@ -73,7 +66,6 @@ pub fn LoginForm(
           name="username_or_email"
           class="input input-bordered"
           placeholder="Username"
-          prop:disabled=move || disabled.get()
           on:keyup=move |ev: ev::KeyboardEvent| {
               let val = event_target_value(&ev);
               set_name.update(|v| *v = val);
@@ -87,7 +79,11 @@ pub fn LoginForm(
 
       </div>
 
-      <PasswordInput id="password" name="password"/>
+      <PasswordInput
+        id="password"
+        name="password"
+        on_input=move |s| set_password.update(|p| *p = s)
+      />
 
       <button class="btn btn-lg" prop:type="submit" prop:disabled=button_is_disabled>
         "Login"
