a982d7ad349035cf3c858717a77e67066d5bf193
jim-taylor-business <jim.taylor.business@gmail.com>
2023-09-26 13:26:46 +0100

    lep fmt fix

--- src/ui/components/common/nav.rs
+++ src/ui/components/common/nav.rs
@@ -10,12 +10,8 @@ pub fn TopNav() -> impl IntoView {
         <div class="dropdown">
           <label tabindex="0" class="btn btn-ghost btn-circle">
             <Icon icon=Icon::from(ChMenuHamburger) width="1.25rem" height="1.25rem"/>
-            // <Icon icon=icon!(ChMenuHamburger) width="1.25rem" height="1.25rem"/>
           </label>
-          <ul
-            tabindex="0"
-            class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
+          <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52" >
             <li>
               <a>"Homepage"</a>
             </li>
@@ -28,12 +24,10 @@ pub fn TopNav() -> impl IntoView {
       <div class="navbar-end gap-3">
         <button class="btn btn-ghost btn-circle">
           <Icon icon=Icon::from(ChSearch) width="1.25rem" height="1.25rem"/>
-          // <Icon icon=icon!(ChSearch) width="1.25rem" height="1.25rem"/>
         </button>
         <button class="btn btn-ghost btn-circle">
           <div class="indicator">
             <Icon icon=Icon::from(ChBell) width="1.25rem" height="1.25rem"/>
-            // <Icon icon=icon!(ChBell) width="1.25rem" height="1.25rem"/>
             <span class="badge badge-xs badge-primary indicator-item"></span>
           </div>
         </button>
