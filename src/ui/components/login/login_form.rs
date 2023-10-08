51745e3d23b55c6d1a5da7583c9566db728a747d
jim-taylor-business <jim.taylor.business@gmail.com>
2023-10-08 11:08:49 +0100

    ssr error handling and nav

--- src/ui/components/login/login_form.rs
+++ src/ui/components/login/login_form.rs
@@ -4,6 +4,8 @@ use crate::{
 };
 use lemmy_api_common::person::{Login, LoginResponse};
 use leptos::{ev, logging::*, *};
+// use crate::use_query_map;
+use leptos_router::use_query_map;
 use leptos_router::ActionForm;
 
 pub async fn login(form: &Login) -> Result<LoginResponse, LemmyAppError> {
@@ -14,7 +16,7 @@ pub async fn login(form: &Login) -> Result<LoginResponse, LemmyAppError> {
 pub async fn login_form_fn(
   username: String,
   password: String,
-  is_ssr: bool,
+  is_ssr_only: bool,
 ) -> Result<LoginResponse, ServerFnError> {
   use crate::{api::set_cookie_wrapper, lemmy_client::LemmyClient};
   use actix_web::web;
@@ -31,16 +33,33 @@ pub async fn login_form_fn(
   //   extract(|client: web::Data<Client>| async move { client.login(&form).await }).await?;
   let result = login(&form).await;
 
-  if is_ssr {
-    redirect("/");
-  }
-
   match result {
     Ok(res) => match set_cookie_wrapper("jwt", &res.jwt.clone().unwrap().into_inner()[..]).await {
-      Ok(_) => Ok(res),
-      Err(e) => Err(ServerFnError::ServerError(e.to_string())),
+      Ok(_) => {
+        if is_ssr_only {
+          redirect("/");
+        }
+        Ok(res)
+      }
+      Err(e) => {
+        if is_ssr_only {
+          redirect("/login?error=1");
+        }
+        Err(ServerFnError::ServerError(e.to_string()))
+      }
     },
-    Err(err) => Err(ServerFnError::ServerError(err.to_string())),
+    Err(err) => {
+      if is_ssr_only {
+        redirect("/login?error=1");
+        Ok(LoginResponse {
+          jwt: None,
+          registration_created: false,
+          verify_email_sent: false,
+        })
+      } else {
+        Err(ServerFnError::ServerError(err.to_string()))
+      }
+    }
   }
 }
 
@@ -51,10 +70,23 @@ pub fn LoginForm() -> impl IntoView {
   let error = create_rw_signal::<Option<String>>(None);
   let (disabled, _set_disabled) = create_signal(false);
 
-  #[cfg(feature = "ssr")]
-  let is_ssr = create_rw_signal::<bool>(true);
+  let is_ssr_only = create_rw_signal::<bool>(true);
+
+  let query = use_query_map();
+  error.set(
+    match query
+      .with(|q| q.get("error").and_then(|page| page.parse::<i64>().ok()))
+      .unwrap_or(0)
+    {
+      1 => Some(
+        "An error occurred while trying to login. Please try again or come back later.".to_string(),
+      ),
+      _x => None,
+    },
+  );
+
   #[cfg(not(feature = "ssr"))]
-  let is_ssr = create_rw_signal::<bool>(false);
+  is_ssr_only.set(false);
 
   let _button_is_disabled =
     Signal::derive(move || disabled.get() || password.get().is_empty() || name.get().is_empty());
@@ -74,35 +106,21 @@ pub fn LoginForm() -> impl IntoView {
       error.set(Some(e.to_string()));
     }
   });
-  let login_form_action = create_server_action::<LoginFormFn>();
-
-  let authenticated = use_context::<RwSignal<bool>>().unwrap_or(create_rw_signal(false));
-
-  create_effect(move |_| match login_form_action.value().get() {
-    None => {}
-    Some(Ok(_o)) => {
-      authenticated.set(true);
-      let navigate = leptos_router::use_navigate();
-      navigate("/", Default::default());
-    }
-    Some(Err(e)) => {
-      error.set(Some(e.to_string()));
-    }
-  });
 
   view! {
+    {move || {
+        error
+            .get()
+            .map(|err| {
+                view! {
+                  <div class="alert alert-error">
+                    <span>{err}</span>
+                  </div>
+                }
+            })
+    }}
+
     <ActionForm action=login_form_action>
-      {move || {
-          error
-              .get()
-              .map(|err| {
-                  view! {
-                    <div class="alert">
-                      <span>{err}</span>
-                    </div>
-                  }
-              })
-      }}
       <input
         name="username"
         type="text"
@@ -137,7 +155,8 @@ pub fn LoginForm() -> impl IntoView {
         }
 
         class="input input-bordered"
-      /> <input name="is_ssr" type="hidden" value=format!("{}", is_ssr.get())/>
+      />
+      <input name="is_ssr_only" type="hidden" value=move || format!("{}", is_ssr_only.get())/>
       <button type="submit" class="btn">
         "Login"
       </button>
