327dab185a2a99e6ce9d60ebb0aafa3f3a67c3f8
jim-taylor-business <jim.taylor.business@gmail.com>
2023-10-12 17:28:36 +0100

    remove comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: {
     files: ["*.html", "./src/**/*.rs"],
   },
-  // theme: {
-  //   extend: {},
-  // },
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["light", "dark", "retro"],
