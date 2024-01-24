e971be530c3aaf83a26135193eb1467b309bf2bb
SleeplessOne1917 <insomnia-void@protonmail.com>
2024-01-23 20:01:52 -0500

    Update TextInput to take advantage of icon change

--- src/ui/components/common/text_input.rs
+++ src/ui/components/common/text_input.rs
@@ -22,6 +22,8 @@ pub fn TextInput(
 ) -> impl IntoView {
   let show_password = RwSignal::new(false);
   let for_id = id.get().clone();
+  let eye_icon =
+    Signal::derive(move || with!(|show_password| if *show_password { EyeSlash } else { Eye }));
 
   view! {
     <div class="relative w-full !mt-8">
@@ -48,9 +50,7 @@ pub fn TextInput(
           class="btn btn-ghost btn-sm btn-circle absolute end-1 bottom-2 text-accent"
           on:click=move |_| update!(| show_password | * show_password = !* show_password)
         >
-          <Show when=move || show_password.get() fallback=move || view! { <Icon icon=Eye/> }>
-            <Icon icon=EyeSlash/>
-          </Show>
+          <Icon icon=eye_icon/>
         </button>
       </Show>
       <label
