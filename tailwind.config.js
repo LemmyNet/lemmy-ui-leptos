6a172ec338568884a622eff007c7967092049e5e
jim <jim.taylor.business@gmail.com>
2024-02-20 12:49:23 +0000

    pretty config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        '3xl': '1920px',
-        '4xl': '2560px',
-        '5xl': '3840px',
-        '6xl': '5120px',
-        '7xl': '8640px',
+        "3xl": "1920px",
+        "4xl": "2560px",
+        "5xl": "3840px",
+        "6xl": "5120px",
+        "7xl": "8640px",
       },
     },
   },
