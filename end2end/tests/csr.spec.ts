b15ea48f049e0ffc16748a725d1f2c05f8c7e436
jim <jim.taylor.business@gmail.com>
2024-03-24 15:18:44 +0000

    e2e tests

---
+++ end2end/tests/csr.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect,  } from "@playwright/test";
+import { loginLogoutTest, persistLanguageTest, persistThemeTest } from "./common";
+
+test.beforeEach(async ({ context }) => {
+  await context.route("*/**/serverfn/*", async route => {
+    await route.abort();
+    throw "CSR - Error - Server function request - " + route.toString();
+  });
+});
+
+// doesn't fail on CSR because of interceptor above slows down logins
+test("show home, login, logout multiple times", async ({ page }) => {
+  await page.goto("/");
+  let iterations = 3
+  while (iterations--) {
+    await loginLogoutTest({ page });
+  }
+});
+
+test("persist language selection between sessions", async ({ page }) => {
+  await persistLanguageTest({ page });
+});
+
+test("persist theme selection between sessions", async ({ page }) => {
+  await persistThemeTest({ page });
+});
---
+++ end2end/tests/ssr.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "@playwright/test";
+import { loginLogoutTest, persistLanguageTest, persistThemeTest } from "./common";
+
+test("show home, login, logout multiple times", async ({ page }) => {
+  await page.goto("/");
+  let iterations = 3
+  while (iterations--) {
+    await loginLogoutTest({ page });
+  }
+});
+
+test("persist language selection between sessions", async ({ page }) => {
+  await persistLanguageTest({ page });
+});
+
+test("persist theme selection between sessions", async ({ page }) => {
+  await persistThemeTest({ page });
+});
