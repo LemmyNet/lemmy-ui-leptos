92eb2c4bd29cdeb6e8103cc7e93d72fb09f46872
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-05-17 20:19:32 -0400

    Fix types in test

--- end2end/tests/common.ts
+++ end2end/tests/common.ts
@@ -1,22 +1,22 @@
-import { expect } from "@playwright/test";
+import { Page, expect } from "@playwright/test";
 
-export async function showHome({ page }) {
+export async function showHome(page: Page) {
   await page.goto("/");
 
   await expect(
-    page.locator("a").getByText("Login", { exact: true }).first(),
+    page.locator("a").getByText("Login", { exact: true }).first()
   ).toHaveText("Login");
 }
 
-export async function loginLogoutTest({ page }) {
+export async function loginLogoutTest(page: Page) {
   await expect(
-    page.locator("a").getByText("Login", { exact: true }).first(),
+    page.locator("a").getByText("Login", { exact: true }).first()
   ).toHaveText("Login");
 
   await page.locator("a").getByText("Login", { exact: true }).first().click();
 
   await expect(page.getByRole("button").getByText("Login").first()).toHaveText(
-    "Login",
+    "Login"
   );
 
   await page.getByLabel("Username", { exact: true }).fill("lemmy");
@@ -30,39 +30,13 @@ export async function loginLogoutTest({ page }) {
   await page.getByRole("button").getByText("Logout", { exact: true }).click();
 }
 
-// export async function persistLanguageTest({ page }) {
-//   await page.goto("/");
-
-//   await expect(page.locator("a").getByText("Modlog").first()).toHaveText(
-//     "Modlog",
-//   );
-
-//   await expect(
-//     page.locator("summary").getByText("Lang", { exact: true }),
-//   ).toBeVisible();
-
-//   await page.locator("summary").getByText("Lang", { exact: true }).click();
-//   await page.getByRole("button").getByText("FR", { exact: true }).click();
-
-//   await expect(page.locator("a").getByText("fr Modlog").first()).toHaveText(
-//     "fr Modlog",
-//   );
-
-//   await page.goto("https://join-lemmy.org");
-//   await page.goto("/");
-
-//   await expect(page.locator("a").getByText("fr Modlog").first()).toHaveText(
-//     "fr Modlog",
-//   );
-// }
-
-export async function persistThemeTest({ page }) {
+export async function persistThemeTest(page: Page) {
   await page.goto("/");
 
   await expect(page.locator("html")).toHaveAttribute("data-theme");
 
   await expect(
-    page.locator("summary").getByText("Theme", { exact: true }),
+    page.locator("summary").getByText("Theme", { exact: true })
   ).toBeVisible();
 
   await page.locator("summary").getByText("Theme", { exact: true }).click();
--- end2end/tests/hydrate.spec.ts
+++ end2end/tests/hydrate.spec.ts
@@ -2,17 +2,17 @@ import { test } from "@playwright/test";
 import { loginLogoutTest, persistThemeTest, showHome } from "./common";
 
 test("show home", async ({ page }) => {
-  await showHome({ page });
+  await showHome(page);
 });
 
 test("login, logout multiple times", async ({ page }) => {
   await page.goto("/");
   let iterations = 3;
   while (iterations--) {
-    await loginLogoutTest({ page });
+    await loginLogoutTest(page);
   }
 });
 
 test("persist theme selection between sessions", async ({ page }) => {
-  await persistThemeTest({ page });
+  await persistThemeTest(page);
 });
