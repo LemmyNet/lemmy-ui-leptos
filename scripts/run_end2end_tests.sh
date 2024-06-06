b388a3dd660089098f2f590e08df93e6d5e1891a
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-06-06 10:43:40 -0400

    Fix env var typo

--- scripts/run_end2end_tests.sh
+++ scripts/run_end2end_tests.sh
@@ -1,7 +1,7 @@
 # This script assumes docker can be run as a non-root user. See the following docs if the script fails because you need to use sudo:
 # https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user
 
-CONTANIER='end2end'
+CONTAINER='end2end'
 DOCKERFILE_PATH='end2end/docker-compose.yml'
 
 # define some colors to use for output
@@ -10,8 +10,8 @@ NC='\033[0m'
 
 # kill and remove any running containers
 cleanup () {
-  docker compose -p $CONTANIER kill
-  docker compose -p $CONTANIER rm -f --all
+  docker compose -p $CONTAINER kill
+  docker compose -p $CONTAINER rm -f --all
 }
 
 # catch unexpected failures, do cleanup and output an error message
@@ -19,7 +19,7 @@ trap 'cleanup ; printf "${RED}Tests Failed For Unexpected Reasons${NC}\n"'\
   HUP INT QUIT PIPE TERM
 
 # build and run the composed services
-docker compose -p $CONTANIER -f $DOCKERFILE_PATH build
-docker compose -p $CONTANIER -f $DOCKERFILE_PATH run $CONTANIER
+docker compose -p $CONTAINER -f $DOCKERFILE_PATH build
+docker compose -p $CONTAINER -f $DOCKERFILE_PATH run $CONTAINER
 
 cleanup
