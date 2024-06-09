18b9185b969d68f1470bc3315e7110ffad5f907f
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-06-09 19:26:25 -0400

    Remove counts badges

--- src/ui/components/common/counts_badge.rs
+++
@@ -1,6 +0,0 @@
-use leptos::*;
-
-#[component]
-pub fn CountsBadge(children: Children) -> impl IntoView {
-  view! { <span class="badge badge-neutral inline-block whitespace-nowrap">{children()}</span> }
-}
