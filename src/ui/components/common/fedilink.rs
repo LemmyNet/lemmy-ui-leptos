6ccbace92b8d6ca3477bd73620abb093b49fabb5
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-07-21 22:28:23 -0400

    Add label to fedilinks

--- src/ui/components/common/fedilink.rs
+++ src/ui/components/common/fedilink.rs
@@ -5,8 +5,10 @@ use leptos::*;
 pub fn Fedilink(#[prop(into)] href: TextProp) -> impl IntoView {
   // Need to make this a variable since using a literal makes leptos expect a format string
   let class = "[@media(hover:hover){&:hover}]:animate-color-cycle active:animate-color-cycle focus:animate-color-cycle cursor-pointer";
+
+  let label = "View content on origin instance";
   view! {
-    <a href=href class=class>
+    <a href=href class=class title=label aria-label=label>
       <Icon icon=IconType::Fediverse/>
     </a>
   }
