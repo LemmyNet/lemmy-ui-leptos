9f6312814c7348abda3ab4a8c9cd6ba4f3ff865e
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-08-27 12:43:39 -0400

    Some more style tweaks

--- src/ui/components/post/post_listings.rs
+++ src/ui/components/post/post_listings.rs
@@ -5,7 +5,7 @@ use leptos::*;
 #[component]
 pub fn PostListings(#[prop(into)] posts: MaybeSignal<Vec<PostView>>) -> impl IntoView {
   view! {
-    <ul class="divide-y divide-neutral sm:overflow-y-auto min-h-0 h-fit sm:h-full mb-14 sm:mb-0">
+    <ul class="divide-y divide-neutral sm:overflow-y-auto min-h-0 h-fit sm:h-full mb-14 sm:mb-4">
       <For each=move || posts.get() key=|pv| pv.post.id let:pv>
         <li class="py-4 h-fit">
           <PostListing post_view=&pv />
