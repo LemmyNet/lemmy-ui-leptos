9d7a97ba67431a5e3067aad33118f6b99b4b077d
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-11-07 16:33:09 -0500

    Revert "Create helper macro to cut down on repitition"
    This reverts commit edead988204dc46729eab43bbde8870e0498e559.

--- src/ui/components/common/markdown_content.rs
+++ src/ui/components/common/markdown_content.rs
@@ -1,45 +1,27 @@
 use crate::utils::markdown_to_html;
 use leptos::*;
 
-macro_rules! make_element_class {
-  ($selector:literal: $($class:literal),+) => {
-      const_str::join!(&[$(const_str::concat!("[&_", $selector, "]:", $class),)*], " ")
-  };
-}
-
 // The class for this is so long that breaking it up into smaller consts makes it easier to deal with.
 const BASE_CLASS: &str = "bg-base-200 mt-4 p-5 rounded space-y-4";
-const H1_CLASS: &str = make_element_class!("h1": "text-2xl", "font-black");
-const H2_CLASS: &str = make_element_class!("h2": "text-2xl", "font-bold");
-const H3_CLASS: &str = make_element_class!("h3": "text-xl", "font-black");
-const H4_CLASS: &str = make_element_class!("h4": "text-xl", "font-bold");
-const H5_CLASS: &str = make_element_class!("h5": "text-lg", "font-black");
-const H6_CLASS: &str = make_element_class!("h5": "text-lg", "font-bold");
-const MONOSPACE: &str = make_element_class!("code": "bg-neutral", "text-neutral-content", "p-1", "rounded-md", "font-medium", "slashed-zero", "text-sm", "font-medium");
+const H1_CLASS: &str = "[&_h1]:text-2xl [&_h1]:font-black";
+const H2_CLASS: &str = "[&_h2]:text-2xl [&_h2]:font-bold";
+const H3_CLASS: &str = "[&_h3]:text-xl [&_h3]:font-black";
+const H4_CLASS: &str = "[&_h4]:text-xl [&_h4]:font-bold";
+const H5_CLASS: &str = "[&_h5]:text-lg [&_h5]:font-black";
+const H6_CLASS: &str = "[&_h6]:text-lg [&_h6]:font-bold";
+const MONOSPACE: &str =
+  "[&_code]:font-mono [&_code]:bg-neutral [&_code]:text-neutral-content [&_code]:p-1 [&_code]:rounded-md [&_code]:font-medium [&_code]:slashed-zero [&_code]:text-sm [&_code]:font-medium";
 
-const CLASS: &str = const_str::join!(
-  &[BASE_CLASS, H1_CLASS, H2_CLASS, H3_CLASS, H4_CLASS, H5_CLASS, H6_CLASS, MONOSPACE],
-  " "
-);
+const CLASS_ARRAY_SLICE: &[&str] = &[
+  BASE_CLASS, H1_CLASS, H2_CLASS, H3_CLASS, H4_CLASS, H5_CLASS, H6_CLASS, MONOSPACE,
+];
 
 #[component]
 pub fn MarkdownContent(#[prop(into)] content: TextProp) -> impl IntoView {
   view! {
     <div
-      class=CLASS
+      class=const_str::join!(CLASS_ARRAY_SLICE, " ")
       inner_html=markdown_to_html(content.get().as_str())
     />
-  }
-}
-
-#[cfg(test)]
-mod test {
-  use rstest::rstest;
-
-  #[rstest]
-  #[case("[&_h1]:text-2xl [&_h1]:font-black", make_element_class!("h1": "text-2xl", "font-black"))]
-  #[case("[&_code]:bg-neutral [&_code]:text-neutral-content [&_code]:p-1", make_element_class!("code": "bg-neutral", "text-neutral-content", "p-1"))]
-  fn class_test(#[case] expected: &str, #[case] result: &str) {
-    assert_eq!(expected, result);
   }
 }
