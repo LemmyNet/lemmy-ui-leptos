64e69960ef28502486f226bc7b6e4b4cbbe2be51
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-11-11 23:34:08 -0500

    Fix formatting failure

--- src/ui/components/common/markdown_content.rs
+++ src/ui/components/common/markdown_content.rs
@@ -3,10 +3,5 @@ use leptos::*;
 
 #[component]
 pub fn MarkdownContent(#[prop(into)] content: TextProp) -> impl IntoView {
-  view! {
-    <div
-      class="markdown-content"
-      inner_html=markdown_to_html(content.get().as_str())
-    />
-  }
+  view! { <div class="markdown-content" inner_html=markdown_to_html(content.get().as_str()) /> }
 }
