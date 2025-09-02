03a311ed42fa090e335328ada679ce4a03bd2de0
Jeff Johnson <219334086+jeff-johnson-tech@users.noreply.github.com>
2025-09-02 13:19:25 -0700

    Add initial Create Community Page

---
+++ src/ui/components/communities/create_community_page.rs
@@ -0,0 +1,11 @@
+use leptos::prelude::*;
+use leptos_fluent::move_tr;
+
+#[component]
+pub fn CreateCommunityPage() -> impl IntoView {
+  view! {
+    <main class="mx-auto">
+      <h2 class="p-6 text-4xl">{move_tr!("create-community")}</h2>
+    </main>
+  }
+}
